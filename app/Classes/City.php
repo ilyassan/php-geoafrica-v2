6805bb1189e62e6b9a87c44792a19f2c7ad162f6
ilyass anida <anidailyass@gmail.com>
2024-12-27 21:29:44 +0100

    #9 Add the City class attributes.

--- app/Classes/City.php
+++ app/Classes/City.php
@@ -1,6 +1,21 @@
 <?php
     class City extends BaseClass {
 
+        private $id;
+        private $name;
+        private $country_id;
+        private $is_capital;
+        private $is_showed;
+
+        public function __construct($id, $name, $country_id, $is_capital, $is_showed)
+        {
+            $this->id = $id;
+            $this->name = $name;
+            $this->country_id = $country_id;
+            $this->is_capital = $is_capital;
+            $this->is_showed = $is_showed;
+        }
+
         public static function findCitiesByCountryId($countryId){
             $sql = "SELECT * FROM cities WHERE id_country = :id_country";
             self::$db->query($sql);
