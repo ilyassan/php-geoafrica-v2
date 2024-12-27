ff58449f9dc654d564a884b461fe3805e2986799
taha <mlaikitaha29@gmail.com>
2024-12-27 20:17:08 +0100

    #66 adjust the delete  function

--- app/Pages/CountryDetailsPage.php
+++ app/Pages/CountryDetailsPage.php
@@ -1,93 +1,97 @@
 <?php
-    class CountryDetailsPage extends BasePage
+class CountryDetailsPage extends BasePage
+{
+    public function index($countryId)
     {
-        public function index($countryId)
-        {
-    
-            $country = Country::findWithLanguages($countryId);
-    
-            $cities = City::findCitiesByCountryId($countryId);
-            $showedCities = [];
-            $unShowedCities = [];
-            foreach($cities as $city){
-                if ($city["is_showed"]) {
-                    $showedCities[] = $city;
-                }else{
-                    $unShowedCities[] = $city;
-                }
-            }
-    
-            $languages = Language::all();
-            
-            $this->render("country-details", compact("country", "showedCities", "unShowedCities", "languages"));
-        }
 
-        public function update($countryId){
+        $country = Country::findWithLanguages($countryId);
 
-            if (user()->isVisitor()) {
-                redirect("");
-            }
-          
-            $population = filter_input(INPUT_POST,"population",FILTER_SANITIZE_SPECIAL_CHARS);
-            $language_id = filter_input(INPUT_POST,"id_language",FILTER_SANITIZE_NUMBER_INT);
-
-            $population = trim($population);
-            $language_id = trim($language_id);
-
-            if(empty($population) || empty($language_id)){
-                flash("error","All field are required");
-                redirect("country/$countryId");
-            }
-            $country = Country::find($countryId);
-            if(!$country){
-                flash("error","No Country found with this id");
-                redirect("");
-            }
-            $country->setPopulation($population);
-            $country->setLanguageId($language_id);
-            if($country->update()){
-                flash("success","Country updated successfully");
-                redirect("country/$countryId");
+        $cities = City::findCitiesByCountryId($countryId);
+        $showedCities = [];
+        $unShowedCities = [];
+        foreach ($cities as $city) {
+            if ($city["is_showed"]) {
+                $showedCities[] = $city;
+            } else {
+                $unShowedCities[] = $city;
             }
         }
 
-        public function delete($countryId){
-            $countryId = htmlspecialchars($countryId);
-            $exist = Country::find($countryId);
-            if(!$exist){
-                flash("error","Country does not exist!");
-                redirect("");
-            }
-            $result = $exist->deleteById($countryId);
-            if($result){
-                flash("success","Country deleted successfully :)");
-                redirect("");
-            }
-        }
-    
-        public function addCityToCountry() {
-            $input = file_get_contents('php://input');
-            $data = json_decode($input, true);
-            
-            $cityId = intval($data["id"]);
+        $languages = Language::all();
 
-            if (City::attachCityToCountry($cityId)) {
-                echo json_encode(["success" => true, "id_received" => $cityId]);
-            }else{
-                echo json_encode(["error" => true, "id_received" => $cityId]);
-            }
+        $this->render("country-details", compact("country", "showedCities", "unShowedCities", "languages"));
+    }
+
+    public function update($countryId)
+    {
+
+        if (user()->isVisitor()) {
+            redirect("");
         }
 
-        public function removeCityFromCountry() {
-            $input = file_get_contents('php://input');
-            $data = json_decode($input, true);
-            
-            $cityId = intval($data["id"]);
+        $population = filter_input(INPUT_POST, "population", FILTER_SANITIZE_SPECIAL_CHARS);
+        $language_id = filter_input(INPUT_POST, "id_language", FILTER_SANITIZE_NUMBER_INT);
 
-            if (City::unattachCityToCountry($cityId)) {
-                echo json_encode(["success" => true, "id_received" => $cityId]);
-            }else{
-                echo json_encode(["error" => true, "id_received" => $cityId]);
-            }
+        $population = trim($population);
+        $language_id = trim($language_id);
+
+        if (empty($population) || empty($language_id)) {
+            flash("error", "All field are required");
+            redirect("country/$countryId");
+        }
+        $country = Country::find($countryId);
+        if (!$country) {
+            flash("error", "No Country found with this id");
+            redirect("");
+        }
+        $country->setPopulation($population);
+        $country->setLanguageId($language_id);
+        if ($country->update()) {
+            flash("success", "Country updated successfully");
+            redirect("country/$countryId");
         }
     }
+
+    public function delete($countryId)
+    {
+        $countryId = htmlspecialchars($countryId);
+        $country = Country::find($countryId);
+        if (!$country) {
+            flash("error", "Country does not exist!");
+            redirect("");
+        }
+        $country->setIsShowed(0);
+        if ($country->update()) {
+            flash("success", "Country delete successfully!");
+            redirect("");
+        }
+    }
+
+    public function addCityToCountry()
+    {
+        $input = file_get_contents('php://input');
+        $data = json_decode($input, true);
+
+        $cityId = intval($data["id"]);
+
+        if (City::attachCityToCountry($cityId)) {
+            echo json_encode(["success" => true, "id_received" => $cityId]);
+        } else {
+            echo json_encode(["error" => true, "id_received" => $cityId]);
+        }
+    }
+
+    public function removeCityFromCountry()
+    {
+        $input = file_get_contents('php://input');
+        $data = json_decode($input, true);
+
+        $cityId = intval($data["id"]);
+
+        if (City::unattachCityToCountry($cityId)) {
+            echo json_encode(["success" => true, "id_received" => $cityId]);
+        } else {
+            echo json_encode(["error" => true, "id_received" => $cityId]);
+        }
+    }
+}
