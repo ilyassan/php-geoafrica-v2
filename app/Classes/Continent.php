dc042b3e8b0c5a075d76ef33145fcc7760b0e739
ilyass anida <anidailyass@gmail.com>
2024-12-27 22:23:29 +0100

    Update the UML Class Diagram.

--- app/Classes/Continent.php
+++ app/Classes/Continent.php
@@ -1,6 +1,16 @@
 <?php
     class Continent extends BaseClass
     {
+        private $id;
+        private $name;
+        private $number_of_countries;
+
+        public function __construct($id, $name, $number_of_countries)
+        {
+            $this->id = $id;
+            $this->name = $name;
+            $this->number_of_countries = $number_of_countries;
+        }
 
         public static function all() {
             $sql = "SELECT * FROM continents";
---
+++ app/Classes/Role.php
@@ -0,0 +1,12 @@
+<?php
+    class Role extends BaseClass
+    {
+        private $id;
+        private $name;
+
+        public function __construct($id, $name)
+        {
+            $this->id = $id;
+            $this->name = $name;
+        }
+    }
